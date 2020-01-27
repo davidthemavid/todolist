c64b5049efc56ed6265112cb93201bca0931cfbd
David Gold <david.the.mavid@gmail.com>
2020-01-27 09:44:08 -0800

    add stylesheet & made available with express

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let items = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   let today = new Date();
