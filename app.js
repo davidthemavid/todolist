2eea75970a391ef61bc9046b2048de6c656042d7
David Gold <david.the.mavid@gmail.com>
2020-01-29 16:56:03 -0800

    install request; add api call to darksky api

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 const config = require("./config.js");
 const key = config.key;
+let port = process.env.PORT || 3000;
 
 //https://darksky.net/dev/docs
-let lat = "";
-let long = "";
+//vancouver lat/long
+let lat = "49.2827";
+let long = "-123.1207";
 let darkSky = `https://api.darksky.net/forecast/${key}/${lat},${long}`;
-let port = process.env.PORT || 3000;
+//console.log(darkSky);
+
+request(`${darkSky}`, { json: true }, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(body);
+  }
+});
 
 const app = express();
 let items = [];
@@ -15,6 +26,8 @@ let items = [];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/");
 
 app.get("/", function(req, res) {
   let today = new Date();
